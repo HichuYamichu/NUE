39bc80706080b6f849ebc05b200bb374fc49465f
Hichu Yamichu <HichuYamichu@gmail.com>
2019-02-23 10:34:29 +0100

    switched to ytdl-core-discord

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,6 +1,4 @@
-module.exports  = (client) => {
-
+module.exports = client => {
 	console.log(`Logged in as ${client.user.tag}!`);
 	client.user.setPresence({ game: { name: 'prefix: 🎶' }, status: 'online' });
-
-}
+};
--- src/functions/playFunction.js
+++ src/functions/playFunction.js
@@ -1,21 +1,21 @@
 module.exports = {
-	play(client, guild, song) {
+	async play(client, guild, song) {
 		const serverQueue = client.queue.get(guild.id);
-		const ytdl = require('ytdl-core');
-		if(!song){
+		const ytdl = require('ytdl-core-discord');
+		if (!song) {
 			serverQueue.voiceChannel.leave();
-			client.queue.delete(guild.id)
+			client.queue.delete(guild.id);
 			return;
 		}
-		const stream = ytdl(song.URL, { filter: 'audioonly' });
-		const dispatcher = serverQueue.connection.playStream(stream, { bitrate: 'auto' })
+		const stream = await ytdl(song.URL);
+		const dispatcher = await serverQueue.connection.playOpusStream(stream)
 			.on('end', () => {
-				console.log('song ended')
+				console.log('song ended');
 				serverQueue.songs.shift();
-				this.play(client, guild, serverQueue.songs[0])
+				this.play(client, guild, serverQueue.songs[0]);
 			})
 			.on('error', error => console.log(error));
-			dispatcher.setVolumeLogarithmic(serverQueue.volume/5);
-			serverQueue.textChannel.send(`**Playing:** ${song.title}`)
+		dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+		serverQueue.textChannel.send(`**Playing:** ${song.title}`);
 	}
-}
+};
